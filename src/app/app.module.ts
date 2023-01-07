c30df7112ee581f7fa618ce35cec59e90c4c970d
Mohamed Toure <demahom.toure@gmail.com>
2023-01-07 22:42:37 +0100

    Add the skeleton of the application

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppHeaderComponent } from './pages/app-header/app-header.component';
+import { MatToolbarModule} from "@angular/material/toolbar";
+import { AppBodyComponent } from './pages/app-body/app-body.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AppHeaderComponent,
+    AppBodyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
